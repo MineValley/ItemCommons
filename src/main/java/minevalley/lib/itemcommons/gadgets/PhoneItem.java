98a17ead61fa998e806c49c5a43738c79a1e72ca
Snabeldier <vincegamezspam@gmail.com>
2026-02-07 17:50:22 +0100

    feat: add basis for phone item

---
+++ src/main/java/minevalley/lib/itemcommons/gadgets/PhoneItem.java
@@ -0,0 +1,63 @@
+package minevalley.lib.itemcommons.gadgets;
+
+import lombok.RequiredArgsConstructor;
+import minevalley.core.api.Core;
+import minevalley.lib.itemcommons.abstracts.CustomModelDataItem;
+import minevalley.lib.itemcommons.abstracts.NonSkullItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
+@RequiredArgsConstructor
+public class PhoneItem implements NonSkullItem, CustomModelDataItem {
+
+    public static @Nonnull TextComponent DISPLAY_NAME =
+            Component.text("Geldbeutel", TextColor.color(156, 77, 20))
+                    .decoration(TextDecoration.ITALIC, false);
+    public static @Nonnull TextComponent[] LORE = new TextComponent[]{
+            Component.text("Zur sicheren Aufbewahrung deiner Bankkarten", NamedTextColor.GRAY)
+                    .decoration(TextDecoration.ITALIC, false),
+            Component.text("Wenn du stirbst, können andere Spieler", NamedTextColor.RED)
+                    .decoration(TextDecoration.ITALIC, false),
+            Component.text("Teile deines Bargelds stehlen!", NamedTextColor.RED)
+                    .decoration(TextDecoration.ITALIC, false)
+    };
+
+    @Override
+    public @Nonnull Material material() {
+        return Material.WOLF_ARMOR;
+    }
+
+    @Override
+    public @Nonnull TextComponent displayName() {
+        return null;
+    }
+
+    @Override
+    public @Nonnull TextComponent[] lore() {
+        return new TextComponent[0];
+    }
+
+    @Override
+    public @Nonnull ItemStack asItemStack() {
+        return Core.createItem(material())
+                .setDisplayName(DISPLAY_NAME)
+                .setLore(LORE)
+                .setCustomModelData(customModelData())
+                .setEquippable(null)
+                .setMaxStackSize(1)
+                .build();
+    }
+
+    @Override
+    public int customModelData() {
+        return 0;
+    }
+}
