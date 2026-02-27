2b8fce68991054168503eb5f647fe8448a9dafbb
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-02-27 02:50:39 +0100

    feat: introduce engines

--- src/main/java/minevalley/lib/itemcommons/technology/engines/CombustionEngineItem.java
+++ src/main/java/minevalley/lib/itemcommons/technology/engines/CombustionEngineItem.java
@@ -15,10 +15,22 @@ import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 @RequiredArgsConstructor
-public class CombustionEngineItem implements CustomItem {
+public enum CombustionEngineItem implements CustomItem {
+
+    COMBUSTION_01(3, 40),
+    COMBUSTION_02(3, 60),
+    COMBUSTION_03(4, 80),
+    COMBUSTION_04(4, 100),
+    COMBUSTION_05(4, 120),
+    COMBUSTION_06(4, 150),
+    COMBUSTION_07(5, 200),
+    COMBUSTION_08(6, 250),
+    COMBUSTION_09(8, 300),
+    COMBUSTION_10(8, 400),
+    COMBUSTION_11(12, 500);
 
     private static final String SKULL = "b441088dee91b16f5fd9aee4ea50a4faa979ce44dcc90524e3d2e2a1942e8a97";
-    public static @Nonnull TextComponent DISPLAY_NAME =
+    public static final @Nonnull TextComponent DISPLAY_NAME =
             Component.text("Motor", TextColor.color(95, 104, 117))
                     .decoration(TextDecoration.ITALIC, false);
 
--- src/main/java/minevalley/lib/itemcommons/technology/engines/ElectricMotorItem.java
+++ src/main/java/minevalley/lib/itemcommons/technology/engines/ElectricMotorItem.java
@@ -3,12 +3,18 @@ package minevalley.lib.itemcommons.technology.engines;
 import lombok.RequiredArgsConstructor;
 import minevalley.core.api.Core;
 import minevalley.core.api.utils.CustomItemFlag;
+import minevalley.crafting.api.Crafting;
+import minevalley.crafting.api.ingredient.RecipeIngredient;
+import minevalley.crafting.api.recipe.CustomShapedRecipe;
 import minevalley.lib.itemcommons.CustomItem;
+import minevalley.lib.itemcommons.technology.CopperCoilItem;
+import minevalley.lib.itemcommons.technology.ScrewItem;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -18,24 +24,35 @@ import javax.annotation.Nonnull;
 public class ElectricMotorItem implements CustomItem {
 
     private static final String SKULL = "fb9051485e15700418022e38a5db28a049a2e627c5ffa56fd313bcdead406a8e";
-    public static @Nonnull TextComponent DISPLAY_NAME =
+    public static final @Nonnull TextComponent DISPLAY_NAME =
             Component.text("Elektromotor", TextColor.color(95, 104, 117))
                     .decoration(TextDecoration.ITALIC, false);
 
-    private final int level;
-    private final int horsepower;
+    private final int kilowatts;
 
     @Override
     public @Nonnull ItemStack asItemStack() {
         return Core.createItem(SKULL)
                 .setDisplayName(DISPLAY_NAME)
-                .setLore(NamedTextColor.GRAY, level + ". Stufe", horsepower + " PS")
+                .setLore(NamedTextColor.GRAY, kilowatts + " kW")
                 .addCustomItemFlags(CustomItemFlag.PREVENT_PLACING)
                 .setMaxStackSize(1)
                 .build();
     }
 
     public static void registerRecipes() {
-        // logic for registering recipes will go here
+        for (int i = 0; i < 293; i++) {
+            final CopperCoilItem coil = new CopperCoilItem(i);
+            final CustomShapedRecipe current = Crafting.createCustomRecipe(new ElectricMotorItem(i + 1).asItemStack(),
+                    new String[]{"SCRCS", "BAIAB", "SCTCS"}, i * 20_000);
+            current.mapIngredient('S', new RecipeIngredient(ScrewItem.SCREW_ITEM));
+            current.mapIngredient('C', new RecipeIngredient(CopperCoilItem.getByLevel(i + 1)));
+            current.mapIngredient('R', new RecipeIngredient(Material.REPEATER));
+            current.mapIngredient('B', new RecipeIngredient(Material.IRON_BARS));
+            current.mapIngredient('A', new RecipeIngredient(Material.BREEZE_ROD));
+            current.mapIngredient('I', new RecipeIngredient(Material.IRON_BLOCK));
+            current.mapIngredient('T', new RecipeIngredient(Material.COMPARATOR));
+            current.register();
+        }
     }
 }
