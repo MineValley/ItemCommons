10fe43f5793f359bd54a728481afdeda35a8a417
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-06-13 23:05:59 +0200

    chore: simplify master key

--- src/main/java/eu/minevalley/itemcommons/gadgets/MasterKeyItem.java
+++ src/main/java/eu/minevalley/itemcommons/gadgets/MasterKeyItem.java
@@ -5,7 +5,6 @@ import eu.minevalley.core.api.user.OnlineUser;
 import eu.minevalley.itemcommons.CustomItem;
 import eu.minevalley.itemcommons.ItemCommons;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -14,41 +13,38 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.NonNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 @SuppressWarnings("unused")
-public class MasterKeyItem implements CustomItem {
+public class MasterKeyItem implements CustomItem  {
 
-    public static @Nonnull Material MATERIAL = Material.NETHER_STAR;
-    public static final NamespacedKey ITEM_MODEL = NamespacedKey.fromString("minevalley:tool/key");
-    public static @Nonnull TextComponent DISPLAY_NAME =
-            Component.text("Universalschlüssel", TextColor.color(70, 178, 224))
-                    .decoration(TextDecoration.ITALIC, false);
-    public static @Nonnull TextComponent[] LORE = new TextComponent[]{
-            Component.text("Öffnet sämtliche Spielersicherungen", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
-            Component.text("Zum Entfernen, Schlüssel ", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false)
-                    .append(Component.text("droppen", NamedTextColor.RED))
-                    .append(Component.text("!", NamedTextColor.GRAY))};
+    public static final ItemStack KEY = ItemCommons.core().createItem(Material.NETHER_STAR)
+            .setDisplayName(Component.text("Universalschlüssel", TextColor.color(70, 178, 224))
+                    .decoration(TextDecoration.ITALIC, false))
+            .setLore(
+                    Component.text("Öffnet sämtliche Spielersicherungen", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
+                    Component.text("Zum Entfernen, Schlüssel ", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false)
+                            .append(Component.text("droppen", NamedTextColor.RED))
+                            .append(Component.text("!", NamedTextColor.GRAY)))
+            .addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP)
+            .addCustomItemFlags(CustomItemFlag.KEEP_IN_INVENTORY, CustomItemFlag.PREVENT_DROPPING,
+                    CustomItemFlag.PREVENT_CRAFTING, CustomItemFlag.TEMPORARY)
+            .setItemModel(NamespacedKey.fromString("minevalley:tool/key"))
+            .setMaxStackSize(1)
+            .build();
 
     @Override
     public @Nonnull ItemStack asItemStack() {
-        return ItemCommons.core().createItem(MATERIAL)
-                .setDisplayName(DISPLAY_NAME)
-                .setLore(LORE)
-                .addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP)
-                .addCustomItemFlags(CustomItemFlag.KEEP_IN_INVENTORY, CustomItemFlag.PREVENT_DROPPING,
-                        CustomItemFlag.PREVENT_CRAFTING, CustomItemFlag.TEMPORARY)
-                .setItemModel(ITEM_MODEL)
-                .setMaxStackSize(1)
-                .build();
+        return KEY.clone();
     }
 
     @Contract(value = "null -> false", pure = true)
     public static boolean isMasterKey(@Nullable ItemStack stack) {
         if (stack == null) return false;
-        return new MasterKeyItem().asItemStack().isSimilar(stack);
+        return KEY.isSimilar(stack);
     }
 
     public static boolean isHoldingMasterKey(@Nonnull OnlineUser user) {
